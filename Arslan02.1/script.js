54a7355c3c60baf128878d9f86ad3b53e97a1f2e
ArslanYan <156071424+ArslanYan@users.noreply.github.com>
2024-02-01 12:53:38 +0500

    Add files via upload

---
+++ Arslan02.1/script.js
@@ -0,0 +1,35 @@
+// function hello(a) {
+//     console.log('Hello ' + a);
+// }
+
+// hello('Arslan')
+
+
+// for(let i = 0; i <= 9999999999; i++){
+//    console.log(i);
+// }
+// function hello(a, b, c, d) {
+//     console.log('Students: ' + a + ', ' + b +', ' + c  +', ' + d + ', ');
+// }
+// hello('Arslan', 'Ruslan', 'Alice', 'Ivan 16 let')
+
+// let hello = (a, b) =>  a + b
+
+
+// hello(5, 10)
+
+
+
+
+// function sum(a, b) {
+//     console.log(a + b);
+// }
+// sum(2,5)
+
+
+
+let first = prompt('Введите первое число')
+let second = prtompt('Введите второе число')
+
+
+let hello = (a, b)
