9ef0f451afd94e63ebc1891b3cd10ed53dfaaa8b
ArslanYan <156071424+ArslanYan@users.noreply.github.com>
2024-02-20 22:12:35 +0500

    Add files via upload

---
+++ LISTERINE/script.js
@@ -0,0 +1,25 @@
+const btn1 = document.querySelector('.btn1')
+const btn2 = document.querySelector('.btn2')
+const btn3 = document.querySelector('.btn3')
+const btn4 = document.querySelector('.btn4')
+
+
+const menu = document.querySelector('.menu')
+const photo = document.querySelector('.photo')
+
+
+btn1.addEventListener('click', () => {
+    menu.classList.add('open')
+})
+
+btn2.addEventListener('click', () => {
+    menu.classList.remove('open')
+})
+
+btn3.addEventListener('click', () => {
+    photo.classList.add('open')
+})
+
+btn4.addEventListener('click', () => {
+    photo.classList.remove('open')
+})
