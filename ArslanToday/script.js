f3f7f82a22cc21dc7c3ce9f5f61d0d13b268d86b
ArslanYan <156071424+ArslanYan@users.noreply.github.com>
2024-02-23 09:42:25 +0200

    Add files via upload

---
+++ ArslanToday/script.js
@@ -0,0 +1,62 @@
+//  let a = 123 //number
+// let b = false //false
+// let c = 'asd' //string
+// let d = 123123123123n //bight
+// let f = Symbol //Symbol
+// let g = null //null
+// let h = undefined //undefined
+// let j = {} //Object
+
+
+
+
+let Arslan = {
+    name:'Arslan',
+    surname:'Yanbukhtin',
+    age:12,
+    addres:'TAshkent'
+}
+
+Arslan.age = 555
+Arslan.addres = 'Kyrgizston'
+
+
+delete Arslan.addres
+
+
+
+
+// let massive = ['Banana', 'Apple', 'Orange']
+
+
+
+// console.log(person [1]);
+
+//Метод массива  который толкает элемент в конец массива 
+// massive.push('Grape')
+
+//поп удаляяет последнийц элемент в массиве
+// let pop = massive.pop()
+
+//.shift удаляет первый элемент в массиве
+// let firstEL = massive.shift()
+
+//.unshift кладет эдемент в начало массива  
+// massive.unshift('Strawberry')
+
+// let Tovar = {
+//     name:'Coca Cola',
+//     price: 6000,
+//     weight:0.5,
+//     color:'black'
+// }
+
+
+// let fruits = ['Apple', 'Banana']
+// let vegetables = ['Carrot', 'Potato']
+
+// let food = fruits.concat(vagetables)
+
+// fruits.splice(2,1)
+
+console.log(Arslan);
